2e89c2c12edc2779e9cd3e598ed99cc0360a95d3
lehaanh22042007-rgb <lehaanh22042007@gmail.com>
2026-03-05 13:41:12 +0700

    Create ex14.py

---
+++ chapter3/ex14.py
@@ -0,0 +1,10 @@
+#bài 14
+def count_vowels(s):
+    s = s.lower()
+    count = 0
+    for char in s:
+        if char in "aeiou":
+            count += 1
+    return count
+text = input("Nhập chuỗi: ")
+print("Số nguyên âm:", count_vowels(text))
